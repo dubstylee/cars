02c9a6297d48edf0762405268c45ea5f949335cb
ayenpure <abhishek@uoregon.edu>
2017-11-24 22:52:30 -0800

    adding GUI schematic, refinements pending

--- gui.py
+++ gui.py
@@ -1,12 +1,19 @@
 import Tkinter as tk
+import signal
 from PIL import Image, ImageTk
 from enum import Enum
-from shared import mqtt_client, mqtt_topic, exit_program
+from shared import mqtt_client, mqtt_topic, exit_program, control_c_handler
 
 # Intersection Control System GUI (ICS)
+signal.signal(signal.SIGINT, control_c_handler)
 
 class ICS(tk.Frame):
     labels = []
+    topFrame = None
+    bottomFrame = None
+    carGridFrame = None
+    assertFrame = None    
+    messagesFrame = None    
 
     lanelookup = {
       1 : (0,1, 180),
@@ -15,7 +22,16 @@ class ICS(tk.Frame):
       4 : (2,0, 270)
     }    
 
-    def __init__(self, master, frame, numLanes) :
+    def __init__(self, master, numLanes) :
+        tk.Frame.__init__(self, master)
+        master.title("Intersection Control System")
+       
+        self.topFrame = tk.Frame()
+        self.topFrame.grid(row=1, sticky='NSEW')
+
+        self.bottomFrame = tk.Frame()
+        self.bottomFrame.grid(row=2, sticky='NSEW')
+
         laneNumbering = 1
         numBlocks = numLanes*numLanes
         
@@ -24,13 +40,13 @@ class ICS(tk.Frame):
         # Indices to figure if the block is a quizing zone
         qzs = [0,3]
 
-        tk.Frame.__init__(self, master)
-        master.title("Intersection Control System")
-
+        self.carGridFrame = tk.Frame(self.topFrame)
+        self.carGridFrame.grid(row=1,column=1, sticky='NSEW')
+ 
         # Dumb work to assign label texts
         for i in range(numLanes) :
             for j in range(numLanes) :
-                label = tk.Label(frame);
+                label = tk.Label(self.carGridFrame);
                 label.config(highlightthickness=2, 
                              highlightbackground="black", 
                              highlightcolor="black")
@@ -45,15 +61,36 @@ class ICS(tk.Frame):
 
         # Lane logic
         for key, value in self.lanelookup.iteritems() :
-            actualIndex = value[0]*numLanes + value[1];
+            actualIndex = value[0]*numLanes + value[1]
             print actualIndex
             image = Image.open("orangecar.bmp")
-            resized = image.resize((100,100), Image.ANTIALIAS);
+            resized = image.resize((100,100), Image.ANTIALIAS)
             rotated = resized.rotate(value[2]);
-            photo = ImageTk.PhotoImage(rotated)#.convert('RGB')
-            #self.labels[actualIndex].config(text="Lane:%d" %key)
+            photo = ImageTk.PhotoImage(rotated)            
             self.labels[actualIndex].config(image=photo)
             self.labels[actualIndex].image = photo
+
+        self.assertFrame = tk.Frame(self.topFrame)
+        self.assertFrame.grid(row=1,column=2)
+        self.assertFrame.config(highlightthickness=2, 
+                                highlightbackground="black", 
+                                highlightcolor="black")
+        label = tk.Label(self.assertFrame, width =50)
+        label.config(text="Asserts Label")
+        label.pack()
+        listbox = tk.Listbox(self.assertFrame, width=50)
+        listbox.pack()
+
+        self.messagesFrame = tk.Frame(self.bottomFrame)
+        self.messagesFrame.pack()
+        self.messagesFrame.config(highlightthickness=2, 
+                                  highlightbackground="black", 
+                                  highlightcolor="black") 
+        label = tk.Label(self.messagesFrame, width=100)
+        label.config(text="Messages Label")
+        label.pack()
+        listbox = tk.Listbox(self.messagesFrame, width=100)
+        listbox.pack()
 
 def on_message(client, userdata, msg):
     message = mgs.payload
@@ -64,9 +101,7 @@ ics = None
 def main():
     global ics
     root = tk.Tk()
-    frame = tk.Frame()
-    frame.pack()
-    ics = ICS(root, frame, 4)
+    ics = ICS(root, 4)
 
     mqtt_client.will_set(mqtt_topic, '___Will of GUI___', 0, False)
     mqtt_client.on_message = on_message
