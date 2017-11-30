e4bdbbbf08392175a6ccb6fd8e45b876667c8c13
Brian Williams <williamsbm2000@comcast.net>
2017-11-30 10:35:14 -0800

    pycodestyle fixes

--- car.py
+++ car.py
@@ -279,10 +279,10 @@ def main():
     while True:
         if car.state == Status.MAIN:
             # 30% chance for car to enter QZ
-            #if random.randint(1, 10) <= 3:
+            # if random.randint(1, 10) <= 3:
             send_message("ENTER %d %d" % (car.id, car.lane_id))
             car.state = Status.QUEUED
-        elif car.auto_pilot == True and car.state == Status.PARKED:
+        elif car.auto_pilot and car.state == Status.PARKED:
             # 20% chance for car to re-enter QZ again
             if random.randint(1, 10) <= 2:
                 send_message("ENTER %d %d" % (car.id, car.lane_id))
